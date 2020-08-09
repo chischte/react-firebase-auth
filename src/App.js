7ed440463e4fa2267f1b5657e093017644bb67c9
Michael Wettstein <chischte@gmx.ch>
2020-08-09 13:06:03 +0200

    BKP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-//import Home from "./Home";
-// import Login from "./Login";
-// import SignUp from "./SignUp";
-// import { AuthProvider } from "./Auth";
+import Home from "./Home";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import { AuthProvider } from "./Auth";
 import PrivateRoute from "./PrivateRoute";
 
 
